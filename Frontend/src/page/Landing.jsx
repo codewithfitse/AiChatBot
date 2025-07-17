5b00a318e7e5e5b85b7864c6ff4e7d7d7bceee0a
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 18:28:24 +0300

    MAKE SOME CHANGES ON APP

--- Frontend/src/page/Landing.jsx
+++ Frontend/src/page/Landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Landing = () => {
   return (
     <>
-      <div className="w-full min-h-full landing">
+      <div className="w-full min-h-screen landing">
         <h1 className="text-[30px] text-white">Landing</h1>
       </div>
     </>
