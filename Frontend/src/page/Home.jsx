0aaa2182e2aeddadac3991bf563d71a376f8e419
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 19:09:42 +0300

    CHANGING ON HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -8,8 +8,9 @@ export const Home = () => {
           <h1 className="text-[30px] text-green-600">Home</h1>
         </div>
         <div className="w-[80%] h-auto bg-amber-900">
-          <nav className="w-full h-auto bg-amber-900">
-            <ul className="w-auto flex justify-around">
+          <nav className="w-full h-[50px] bg-amber-800">
+            <div className=""></div>
+            <ul className="w-auto flex justify-around items-center">
               <li className="text-[30px] text-center text-black">Home</li>
               <li className="text-[30px] text-center text-black">Home</li>
               <li className="text-[30px] text-center text-black">Home</li>
