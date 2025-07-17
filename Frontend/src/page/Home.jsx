3e839ae9f21f1807916ae3dd65040c29db2f61fb
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 20:12:17 +0300

    CHANGING ON HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -14,10 +14,17 @@ export const Home = () => {
             <h1 className="text-white text-[20px]">Home</h1>
           </nav>
           <div className="w-full h-full bg-amber-700">
-            <div className="w-full h-full mt-52 flex flex-col items-center">
+            <div className="w-full h-full mt-52 flex flex-col ju items-center space-y-7">
               <h1 className="">Hey, Fitsum. Ready to dive in?</h1>
-              <div className="w-fit h-fit px-5 py-3 rounded-2xl bg-amber-500">
-                <h1 className="">Ask</h1>
+              <div className="w-[80%] h-fit flex items-center px-5 py-3 rounded-[10px] bg-amber-500 space-x-2">
+                <input
+                  type="text"
+                  className="w-full h-[30px] p-2 bg-amber-50 rounded-[5px] placeholder:text-gray-900"
+                  placeholder="Type whats on your mind?"
+                />
+                <button className="px-2 py-1 bg-amber-300 rounded-[5px]">
+                  ✓
+                </button>
               </div>
             </div>
           </div>
