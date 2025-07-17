7a80fef1ee2fd7e701ef51b55f3542d1a3eaf9dd
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 22:14:22 +0300

    CHANGING ON HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -5,17 +5,19 @@ export const Home = () => {
     <>
       <div className="w-full min-h-full flex bg-black text-white">
         <aside className="w-[20%] h-auto bg-gray-950">
-          <h1 className="text-[30px] text-green-600 text-center">Home</h1>
+          <h1 className="text-[15px] lg:text-[30px] text-center">Dashboard</h1>
         </aside>
         <main className="w-full h-full flex flex-col items-center bg-gray-900">
-          <nav className="w-full h-[50px] flex justify-around items-center bg-white/2">
-            <h1 className="text-[20px]">FitseGpt</h1>
-            <h1 className="text-[20px]">Home</h1>
-            <h1 className="text-[20px]">Home</h1>
+          <nav className="w-full h-[25px] lg:h-[50px] flex justify-around items-center text-[10px] lg:text-[20px] bg-white/2">
+            <h1 className="">FitseGpt</h1>
+            <h1 className="">Home</h1>
+            <h1 className="">Home</h1>
           </nav>
           <div className="w-full h-full pb-10 bg-transparent">
-            <div className="w-full h-full mt-40 flex flex-col ju items-center space-y-7">
-              <h1 className="">Hey, User. Ready to dive in?</h1>
+            <div className="w-full h-full mt-20 lg:mt-40 flex flex-col ju items-center space-y-7">
+              <h1 className="text-[20px] lg:text-[30px]">
+                Hey, User. Ready to dive in?
+              </h1>
 
               <div className="chat">
                 <img
@@ -24,20 +26,20 @@ export const Home = () => {
                   srcset=""
                   className="size-[25px] m-4 bg-gray-600"
                 />
-                <p className="w-[90%] text-[18px]">
+                <p className="w-[90%] text-[10px] lg:text-[18px]">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
                   corrupti delectus pariatur debitis sed.
                 </p>
               </div>
 
-              <div className="chat">
+              <div className="chat rounded-[10px]">
                 <img
                   src="logo.webp"
                   alt=""
                   srcset=""
                   className="size-[25px] m-4 bg-gray-600"
                 />
-                <p className="w-[90%] text-[18px]">
+                <p className="w-[90%] text-[10px] lg:text-[18px]">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
                   corrupti delectus pariatur debitis sed. Consectetur natus ad
                   ipsa cumque nostrum exercitationem accusamus eligendi laborum
@@ -45,13 +47,13 @@ export const Home = () => {
                 </p>
               </div>
 
-              <div className="w-[80%] h-fit flex items-center px-5 py-3 rounded-[10px] bg-amber-500 space-x-2">
+              <div className="w-[80%] h-fit flex items-center px-5 py-3 rounded-[10px] bg-gray-950 space-x-2">
                 <input
                   type="text"
-                  className="w-full h-[30px] p-2 bg-amber-50 rounded-[5px] placeholder:text-gray-900"
+                  className="w-full h-[50px] p-3 bg-gray-900 rounded-[5px] placeholder:text-white"
                   placeholder="Type whats on your mind?"
                 />
-                <button className="px-2 py-1 bg-amber-300 rounded-[5px]">
+                <button className="px-3 bg-white text-black text-[30px] rounded-[5px]">
                   ✓
                 </button>
               </div>
