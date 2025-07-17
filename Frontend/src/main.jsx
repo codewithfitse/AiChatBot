65c32d8cf70fa5be5622735ec0478ac203370adc
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 17:58:59 +0300

    MAKE SOME CHANGES ON APP

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { NotFound } from "./page/404.jsx";
+import { Home } from "./page/Home.jsx";
 const router = createBrowserRouter([
-  { path: "/", errorElement: <NotFound></NotFound> },
+  { path: "*", errorElement: <NotFound /> },
+  { path: "/", element: <Home /> },
+  { path: "/", element: <NotFound /> },
+  { path: "/", element: <NotFound /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
--- Frontend/src/page/404.jsx
+++ Frontend/src/page/404.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const NotFound = () => {
   return (
     <div>
-      <h1 className="">Not Found</h1>
+      <h1 className="text-white text-[30px]">Not Found</h1>
     </div>
   );
 };
