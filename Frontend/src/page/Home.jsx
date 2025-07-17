6375bb8469a3eee9eff73e3f538feae369d356f9
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 18:49:09 +0300

    MAKE MERGING TO MAIN

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -3,8 +3,11 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <div>
-        <h1 className="text-[30px] text-white">Home</h1>
+      <div className="w-full min-h-full flex">
+        <div className="w-[25%] h-full bg-amber-700"></div>
+        <div className="w-full h-full">
+          <h1 className="text-[30px] text-white">Home</h1>
+        </div>
       </div>
     </>
   );
