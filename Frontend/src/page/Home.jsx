5572ecb420535afde5571749514e39cd6bd6c5ed
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 19:06:33 +0300

    MAKE MERGING TO MAIN

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -3,10 +3,18 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <div className="w-full min-h-full flex">
-        <div className="w-[25%] h-full bg-amber-700"></div>
-        <div className="w-full h-full">
-          <h1 className="text-[30px] text-white">Home</h1>
+      <div className="w-full min-h-screen flex bg-amber-500">
+        <div className="w-[20%] h-auto bg-amber-950">
+          <h1 className="text-[30px] text-green-600">Home</h1>
+        </div>
+        <div className="w-[80%] h-auto bg-amber-900">
+          <nav className="w-full h-auto bg-amber-900">
+            <ul className="w-auto flex justify-around">
+              <li className="text-[30px] text-center text-black">Home</li>
+              <li className="text-[30px] text-center text-black">Home</li>
+              <li className="text-[30px] text-center text-black">Home</li>
+            </ul>
+          </nav>
         </div>
       </div>
     </>
