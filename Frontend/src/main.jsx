e3b8d20a9f29df66a5d17dd7d12d46b135c4a26b
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-17 17:50:24 +0300

    MAKE SOME CHANGES ON APP

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowser } from "react-router";
-const router = createBrowser();
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { NotFound } from "./page/404.jsx";
+const router = createBrowserRouter([
+  { path: "/", errorElement: <NotFound></NotFound> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>
 );
