358cd9047202216ed062192a484b4d8c7472d280
Fitsum Zerihun <fitsa0909@gmail.com>
2025-09-17 14:43:14 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6 pl-[70px]">
-        <Text variant="h1">OpenAI GPT-3</Text>
+        <Text variant="h1">OpenAI GPT-5</Text>
       </section>
 
       <section className="flex flex-col gap-3 pl-[40px]">
-        <h3 className="text-2xl pl-[40px] ">Noah-GPT-bot</h3>
+        <h3 className="text-2xl pl-[40px] ">Fitse-GPT-bot</h3>
         <div className="lg:w-2/3  mt-[100px]">
           <Chat />
         </div>
